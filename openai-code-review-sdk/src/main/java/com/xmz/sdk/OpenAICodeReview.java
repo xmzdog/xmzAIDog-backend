a956959688e51dd34f1bc8c0d3867557e04de27d
xiangmz <m18871328482_1@163.com>
2025-04-18 10:24:08 +0800

    【feat】新增代码评审

---
+++ openai-code-review-sdk/src/main/java/com/xmz/sdk/OpenAICodeReview.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiangmz
+ * Date: 2025/4/17
+ * Time: 22:06
+ * Description: No Description
+ */
+public class OpenAICodeReview {
+    public static void main(String[] args) {
+
+    }
+}
