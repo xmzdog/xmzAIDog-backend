8b2f99bc9154070af256940f96c7728e766a0424
xiangmz <m18871328482_1@163.com>
2025-04-19 18:54:54 +0800

    [feat]新增ChatGLM 大模型api进行代码评审

--- openai-code-review-sdk/src/main/java/com/xmz/sdk/OpenAICodeReview.java
+++ openai-code-review-sdk/src/main/java/com/xmz/sdk/OpenAICodeReview.java
@@ -1,9 +1,16 @@
 package com.xmz.sdk;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.alibaba.fastjson2.JSON;
+import com.xmz.sdk.model.ChatCompletionRequest;
+import com.xmz.sdk.model.ChatCompletionSyncResponse;
+import com.xmz.sdk.model.Model;
+import com.xmz.sdk.utils.ChatGLMToken;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -34,5 +41,64 @@ public class OpenAICodeReview {
         int exitcode = start.waitFor();
         System.out.println("Exited with code " + exitcode);
         System.out.println("评审代码"+stringBuilder.toString());
+
+        //2. ChatGLM api 代码评审
+        String log = codeReview(stringBuilder.toString());
+        System.out.println("code review：" + log);
+
+
     }
+
+
+    private static String codeReview(String diffCode) throws Exception {
+
+        String apiKeySecret = "3e170b0c86004a7bbc93d456cfb2d645.7OzCVMrF8yBgRjyG";
+        String token = ChatGLMToken.getToken(apiKeySecret);
+
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
+            private static final long serialVersionUID = -7988151926241837899L;
+
+            {
+                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user", diffCode));
+            }
+        });
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        System.out.println("评审结果：" + content.toString());
+
+        ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
+        return response.getChoices().get(0).getMessage().getContent();
+    }
+
+
 }
---
+++ openai-code-review-sdk/src/test/java/com/xmz/sdk/utils/ChatGLMTokenTest.java
@@ -0,0 +1,86 @@
+package com.xmz.sdk.utils;
+
+import com.alibaba.fastjson2.JSON;
+import com.xmz.sdk.model.ChatCompletionSyncResponse;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiangmz
+ * Date: 2025/4/19
+ * Time: 18:26
+ * Description: No Description
+ */
+@SpringBootTest
+public class ChatGLMTokenTest {
+
+    public static void main(String[] args) {
+        String apiKeySecret = "3e170b0c86004a7bbc93d456cfb2d645.7OzCVMrF8yBgRjyG";
+        String token = ChatGLMToken.getToken(apiKeySecret);
+        System.out.println(token);
+    }
+
+    @Test
+    public void test_http() throws IOException {
+        String apiKeySecret = "3e170b0c86004a7bbc93d456cfb2d645.7OzCVMrF8yBgRjyG";
+        String token = ChatGLMToken.getToken(apiKeySecret);
+
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+
+        String code = "1+1";
+
+        String jsonInpuString = "{"
+                + "\"model\":\"glm-4-flash\","
+                + "\"messages\": ["
+                + "    {"
+                + "        \"role\": \"user\","
+                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
+                + "    }"
+                + "]"
+                + "}";
+
+
+        try(OutputStream os = connection.getOutputStream()){
+            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null){
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+        System.out.println(content);
+
+        ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
+        System.out.println(response.getChoices().get(0).getMessage().getContent());
+
+    }
+
+}
